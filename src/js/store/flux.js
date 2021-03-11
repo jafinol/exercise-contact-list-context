1a97ca9363a2ac9efef13a182b2fdf4b66f10330
jafinol <jafinol@gmail.com>
2021-03-11 01:51:27 +0000

    first commit

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,11 +1,20 @@
-const getState = ({ getStore, setStore }) => {
+const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
 			//Your data structures, A.K.A Entities
+			agenda: []
 		},
 		actions: {
 			//(Arrow) Functions that update the Store
 			// Remember to use the scope: scope.state.store & scope.setState()
+			getAgenda: () => {
+				fetch("https://assets.breatheco.de/apis/fake/contact/agenda/finolweb")
+					.then(res => res.json())
+					.then(response => {
+						//console.log(response);
+						setStore({ agenda: response });
+					});
+			}
 		}
 	};
 };
--- src/js/views/Contacts.js
+++ src/js/views/Contacts.js
@@ -1,13 +1,16 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-
+import React, { useState, useEffect, useContext } from "react";
+import { Link, useParams } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 import { ContactCard } from "../component/ContactCard.js";
 import { Modal } from "../component/Modal";
+import PropTypes from "prop-types";
+import { Context } from "../store/appContext";
 
 export const Contacts = () => {
 	const [state, setState] = useState({
 		showModal: false
 	});
+	const { store, actions } = useContext(Context);
 
 	return (
 		<div className="container">
@@ -17,15 +20,23 @@ export const Contacts = () => {
 						Add new contact
 					</Link>
 				</p>
-				<div id="contacts" className="panel-collapse collapse show" aria-expanded="true">
-					<ul className="list-group pull-down" id="contact-list">
-						<ContactCard onDelete={() => setState({ showModal: true })} />
-						<ContactCard />
-						<ContactCard />
-						<ContactCard />
-					</ul>
-				</div>
 			</div>
+
+			<div id="contacts" className="panel-collapse collapse show" aria-expanded="true">
+				<ul className="list-group pull-down" id="contact-list">
+					{store.agenda &&
+						store.agenda.map((item, index) => {
+							return (
+								<ContactCard
+									key={index}
+									contacts={item}
+									onDelete={() => setState({ showModal: true })}
+								/>
+							);
+						})}
+				</ul>
+			</div>
+
 			<Modal show={state.showModal} onClose={() => setState({ showModal: false })} />
 		</div>
 	);
